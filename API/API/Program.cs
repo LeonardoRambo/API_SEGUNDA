12652f4a96c737d11dc9da1a663bae5de410a4d2
Auto Logon <autologon@CSED.LAB>
2024-09-02 21:43:45 -0300

    final da aula

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,6 +1,16 @@
+// minimal API's em C#
+// Testar a API
+// Rest Client  - Extesão VSCODE
+// Postman
+// Insomnia
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Funcionalidade = endponits
+// Resquisição = URL e método/verbo HTTP
+app.MapGet("/", () => "oi!");
+
+app.MapGet("/2", () => "hello 2!");
 
 app.Run();
